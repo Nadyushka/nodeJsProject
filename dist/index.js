b761d60c430b122b5c0613049582483c63d11931
Nadyushka <12nadya11@gmail.com>
2023-05-25 21:12:36 +0300

    add CRUD operation

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,63 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
-const port = 3002;
-app.get('/', (req, res) => {
-    const message = 'Hello World!';
-    res.send(message);
+const port = 5000;
+const products = [{ id: 1, title: 'tomato' }, { id: 2, title: 'apple' }];
+//const addresses = [{id: 1, value: 'Minsk'}, {id: 2, title: 'Grodno'}]
+const parserMiddleware = (0, body_parser_1.default)({});
+app.use(parserMiddleware);
+app.get('/products', (req, res) => {
+    if (req.query.title) {
+        const title = req.query.title;
+        const selectedProduct = products.filter(p => p.title.indexOf(title) > -1);
+        res.send(selectedProduct);
+    }
+    else {
+        res.send(products);
+    }
+});
+app.get('/products/:productId', (req, res) => {
+    const selectedProduct = products.find(p => p.id == +req.params.productId);
+    if (selectedProduct) {
+        res.send(selectedProduct);
+    }
+    else {
+        res.send(404);
+    }
+});
+app.delete('/products/:productId', (req, res) => {
+    if (req.params.productId) {
+        for (let i = 0; i < products.length; i++) {
+            if (products[i].id === +req.params.productId) {
+                products.splice(i, 1);
+                res.send(204);
+                return;
+            }
+        }
+    }
+    else {
+        res.send(404);
+    }
+});
+app.post('/products', (req, res) => {
+    const newProduct = {
+        id: +(new Date()),
+        title: req.body.title
+    };
+    products.push(newProduct);
+    res.status(201).send(products);
+});
+app.put('/products/:productId', (req, res) => {
+    const selectedProduct = products.find(p => p.id == +req.params.productId);
+    if (selectedProduct) {
+        selectedProduct.title = req.body.title;
+        res.send(products);
+    }
+    else {
+        res.send(404);
+    }
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
